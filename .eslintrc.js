bcc12b910b44b11bd1762d24e12b0bb15438b22a
Terry Tsai <terry@recidiviz.org>
2022-10-24 13:59:12 -0700

    remove react imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,7 @@ module.exports = {
   plugins: ["header"],
   rules: {
     "header/header": [2, "../license-header.js"],
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
   },
 };
--- agency-dashboard/src/App.tsx
+++ agency-dashboard/src/App.tsx
@@ -15,7 +15,6 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
 
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import AgencyOverview from "./AgencyOverview";
