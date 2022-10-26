c9ce56bbcc8075531971c6a12a93956caba9e685
Terry Tsai <terry@recidiviz.org>
2022-10-26 11:16:08 -0700

    [Frontend] Implement BaseDatapointsStore class that is shared between our frontend apps (#114)
    * implement Base Datapoints class that is shared between our frontend apps
    
    * fix lint
    
    * remove unnecessary comment

--- agency-dashboard/src/stores/index.ts
+++ agency-dashboard/src/stores/index.ts
@@ -15,6 +15,5 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
 
-export * from "./DatapointsStore";
 export { default as rootStore } from "./RootStore";
 export * from "./StoreProvider";
