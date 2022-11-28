f7b34f999441082fed0ea3180373231dd46697bf
Terry Tsai <terry@recidiviz.org>
2022-11-28 11:57:34 -0800

    fix lint

--- common/components/DataViz/MetricInsights.tsx
+++ common/components/DataViz/MetricInsights.tsx
@@ -16,6 +16,7 @@
 // =============================================================================
 
 import React from "react";
+
 import { Datapoint } from "../../types";
 import {
   MetricInsight,
