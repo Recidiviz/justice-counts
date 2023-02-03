210e5bc7af8d0fc76c434bfd877da05f98fe8ceb
Mahmoud <mahmoud@Mahmouds-MacBook-Pro.local>
2023-02-03 12:45:51 -0500

    Remove await

--- publisher/src/stores/ReportStore.test.tsx
+++ publisher/src/stores/ReportStore.test.tsx
@@ -92,7 +92,7 @@ test("displayed reports", async () => {
     </StoreProvider>
   );
 
-  await act(() => {
+  act(() => {
     runInAction(() => {
       rootStore.reportStore.loadingOverview = false;
       rootStore.reportStore.reportOverviews = mockUnorderedReportsMap;
