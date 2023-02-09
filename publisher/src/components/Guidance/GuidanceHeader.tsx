5c895d2a081f625abe326c26b20cd3e436a22ed9
Mahmoud <mahmoud@Mahmouds-MacBook-Pro.local>
2023-02-09 10:46:56 -0500

    Exploring autorun solution

--- publisher/src/components/Guidance/GuidanceHeader.tsx
+++ publisher/src/components/Guidance/GuidanceHeader.tsx
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
 
+import { autorun } from "mobx";
 import { observer } from "mobx-react-lite";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
@@ -39,6 +40,7 @@ import {
 export const GuidanceHeader = observer(() => {
   const { guidanceStore } = useStore();
   const {
+    metricConfigurationProgressStepsTracker,
     currentTopicID,
     calculateOverallMetricProgress,
     calculateMetricAvailabilityFrequencyProgress,
@@ -111,6 +113,20 @@ export const GuidanceHeader = observer(() => {
       breakdownDefinitionProgress,
     ]
   );
+
+  // useEffect(() => {
+  //   const dispose = autorun(() => {
+  //     handleMetricConfigToastDisplay();
+  //   });
+
+  //   return () => {
+  //     dispose();
+  //   };
+  // }, [
+  //   metricConfigurationProgressStepsTracker[systemMetricKey]?.[
+  //     "Confirm breakdowns"
+  //   ],
+  // ]);
 
   if (!guidanceStore.isInitialized) return null;
 
