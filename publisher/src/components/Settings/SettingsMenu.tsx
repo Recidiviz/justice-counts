65c049627532f9b38079c0eba5e9142ee2c3e31b
Terry Tsai <terry@recidiviz.org>
2023-02-14 16:03:07 -0800

    lint

--- publisher/src/components/Settings/SettingsMenu.tsx
+++ publisher/src/components/Settings/SettingsMenu.tsx
@@ -17,7 +17,7 @@
 
 import { observer } from "mobx-react-lite";
 import React, { Fragment } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { settingsMenuPaths } from "../../pages/Settings";
 import { useStore } from "../../stores";
