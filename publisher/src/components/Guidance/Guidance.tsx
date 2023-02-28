21cc304731cbcdd7cf2924f47e8c8b522a01b346
Mahmoud <mahmoud@Mahmouds-MBP.cable.rcn.com>
2023-02-28 14:06:38 -0600

    Refactor useEffect initialization of report store and metric config store to reduce unnecessary backend calls

--- publisher/src/components/Guidance/Guidance.tsx
+++ publisher/src/components/Guidance/Guidance.tsx
@@ -133,11 +133,9 @@ export const Guidance = observer(() => {
   );
 
   useEffect(() => {
-    const initialize = async () => {
+    const initializeReportStoreUpdateProgress = async () => {
       reportStore.resetState();
       await reportStore.getReportOverviews(agencyId);
-      if (currentTopicID === "METRIC_CONFIG")
-        await metricConfigStore.initializeMetricConfigStoreValues(agencyId);
 
       const hasMinimumOneReport =
         currentTopicID === "ADD_DATA" &&
@@ -166,6 +164,10 @@ export const Guidance = observer(() => {
           agencyId
         );
       }
+    };
+    const initializeMetricConfigStoreUpdateProgress = async () => {
+      await metricConfigStore.initializeMetricConfigStoreValues(agencyId);
+
       if (totalMetrics > 0 && numberOfMetricsCompleted === totalMetrics) {
         saveOnboardingTopicsStatuses(
           {
@@ -177,12 +179,13 @@ export const Guidance = observer(() => {
       }
     };
 
-    if (
-      currentTopicID === "ADD_DATA" ||
-      currentTopicID === "PUBLISH_DATA" ||
-      currentTopicID === "METRIC_CONFIG"
-    )
-      initialize();
+    if (currentTopicID === "ADD_DATA" || currentTopicID === "PUBLISH_DATA") {
+      initializeReportStoreUpdateProgress();
+    }
+
+    if (currentTopicID === "METRIC_CONFIG") {
+      initializeMetricConfigStoreUpdateProgress();
+    }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [agencyId, currentTopicID, numberOfMetricsCompleted]);
