4d17ebae531b21daa228d4dc0b307d1713a6edb9
Michelle Orden <82831800+morden35@users.noreply.github.com>
2023-03-30 11:59:06 -0500

    attempting to add icon to disabled metrics in new section

--- publisher/src/components/DataUpload/UploadErrorsWarnings.tsx
+++ publisher/src/components/DataUpload/UploadErrorsWarnings.tsx
@@ -47,6 +47,7 @@ import {
   Title,
   Wrapper,
 } from ".";
+import { NotReportedIcon } from "./NotReportedIcon";
 import {
   ErrorsWarningsMetrics,
   ErrorWarningMessage,
@@ -76,7 +77,7 @@ export const UploadErrorsWarnings: React.FC<UploadErrorsWarningsProps> = ({
     selectedSystem && systemToTemplateSpreadsheetFileName[selectedSystem];
   const successfulMetricsCount =
     errorsWarningsAndSuccessfulMetrics.successfulMetrics.length;
-  const noMetricUpload =
+  const noMetricUploadCount =
     errorsWarningsAndSuccessfulMetrics.notUploadedMetrics.length;
   /** If there are non-metric errors, include them in the error count */
   const errorCount = nonMetricErrors
@@ -247,13 +248,16 @@ export const UploadErrorsWarnings: React.FC<UploadErrorsWarningsProps> = ({
           </>
         )}
         {/* Metrics with no Uploaded Data */}
-        {noMetricUpload > 0 && (
+        {noMetricUploadCount > 0 && (
           <>
             <SectionHeader>Metrics Not Uploaded</SectionHeader>
             {errorsWarningsAndSuccessfulMetrics.notUploadedMetrics.map(
               (metric) => (
                 <Message key={metric.key} enabled={metric.enabled}>
-                  <MetricTitle>{metric.display_name}</MetricTitle>
+                  <MetricTitle>
+                    {metric.display_name}{" "}
+                    <NotReportedIcon size={16} lighter noTooltip />
+                  </MetricTitle>
                 </Message>
               )
             )}
