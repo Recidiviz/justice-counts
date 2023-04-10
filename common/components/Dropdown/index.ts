29c93dc512061b398c710ec1b2ae6a492108bb5d
ivankishko <mrivankishko@gmail.com>
2023-04-10 16:32:35 +0300

    fix recidiviz header

--- common/components/Dropdown/index.ts
+++ common/components/Dropdown/index.ts
@@ -1,3 +1,7 @@
+// Recidiviz - a data platform for criminal justice reform
+// Copyright (C) 2023 Recidiviz, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
