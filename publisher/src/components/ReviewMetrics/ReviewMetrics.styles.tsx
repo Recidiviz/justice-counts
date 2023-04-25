acb77d050dac01eae7a7e89b7fdbab9057b3fbef
Mahmoud <mahmoud@Mahmouds-MBP.cable.rcn.com>
2023-04-25 17:36:59 -0500

    Reduce width of left panel in the review page and give more space to the datapoints table

--- publisher/src/components/ReviewMetrics/ReviewMetrics.styles.tsx
+++ publisher/src/components/ReviewMetrics/ReviewMetrics.styles.tsx
@@ -44,7 +44,7 @@ export const Container = styled(DataUploadContainer)`
 export const ReviewMetricsWrapper = styled.div<{ hasNoDatapoints: boolean }>`
   ${({ hasNoDatapoints }) => hasNoDatapoints && `height: 100%;`}
   max-width: 100%;
-  padding: 40px 0 128px 612px;
+  padding: 40px 0 128px 520px;
   display: flex;
   flex-direction: row;
   gap: 88px;
@@ -70,7 +70,7 @@ export const Summary = styled.div<{ isFooterVisible?: boolean }>`
   transition: max-height 0.5s ease-in-out;
 
   padding-left: 24px;
-  width: 500px;
+  width: 400px;
   display: flex;
   flex-direction: column;
   background-color: ${palette.solid.white};
