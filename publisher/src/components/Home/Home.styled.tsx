056f642c93cf28ec56b04cd67499e7d48cc77863
Mahmoud <mahmoud@Mahmouds-MBP.atlanticbb.net>
2023-05-29 14:36:40 -0400

    Scaffolding work for home page

--- publisher/src/components/Home/Home.styled.tsx
+++ publisher/src/components/Home/Home.styled.tsx
@@ -15,12 +15,97 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
 
+import {
+  palette,
+  typography,
+} from "@justice-counts/common/components/GlobalStyles";
 import styled from "styled-components/macro";
 
 export const HomeContainer = styled.div`
   width: 100%;
   min-height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const WelcomeUser = styled.div`
+  ${typography.sizeCSS.headline};
+  margin-bottom: 24px;
+`;
+
+export const WelcomeDescription = styled.div`
+  ${typography.sizeCSS.medium};
+  color: ${palette.highlight.grey8};
+  margin-bottom: 64px;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  gap: 48px;
+`;
+
+export const LeftPanelWrapper = styled.div`
+  width: 392px;
+`;
+
+export const OpenTasksContainer = styled.div``;
+
+export const TaskCard = styled.div`
+  width: 644px;
+  padding: 32px;
+  border: 1px solid ${palette.highlight.grey3};
+`;
+
+export const TaskCardTitle = styled.div`
+  ${typography.sizeCSS.large};
+  margin-bottom: 16px;
+`;
+
+export const TaskCardDescription = styled.div`
+  ${typography.sizeCSS.medium};
+  color: ${palette.highlight.grey8};
+  margin-bottom: 48px;
+`;
+
+export const TaskCardActionLinksWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 24px;
+`;
+
+export const TaskCardActionLink = styled.div`
+  color: ${palette.solid.blue};
+
+  &:hover {
+    cursor: pointer;
+    color: ${palette.solid.darkblue};
+  }
+`;
+
+export const Submenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const SubmenuButton = styled.button`
+  ${typography.sizeCSS.medium};
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 24px;
+  padding: 24px;
+  width: 392px;
+  background: transparent;
+  border: 1px solid ${palette.highlight.grey3};
+
+  &:hover {
+    cursor: pointer;
+    background: ${palette.highlight.grey1};
+  }
 `;
--- publisher/src/components/Home/Home.tsx
+++ publisher/src/components/Home/Home.tsx
@@ -15,14 +15,60 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
 
+import { observer } from "mobx-react-lite";
 import React from "react";
 
+import { useStore } from "../../stores";
+import { ReactComponent as GearIcon } from "../assets/gear-icon.svg";
+import { ReactComponent as OpenLinkIcon } from "../assets/open-link-icon.svg";
+import { ReactComponent as SettingsIcon } from "../assets/settings-icon.svg";
 import * as Styled from ".";
 
-export const Home = () => {
+export const Home = observer(() => {
+  const { userStore } = useStore();
+  const userFirstName = userStore.name?.split(" ")[0];
   return (
     <Styled.HomeContainer>
-      Hi, I am going to be a beautiful homepage soon.
+      <Styled.WelcomeUser>Welcome, {userFirstName}</Styled.WelcomeUser>
+      <Styled.WelcomeDescription>
+        See open tasks below
+      </Styled.WelcomeDescription>
+      <Styled.ContentContainer>
+        <Styled.LeftPanelWrapper />
+        <Styled.OpenTasksContainer>
+          <Styled.TaskCard>
+            <Styled.TaskCardTitle>
+              Post-adjudication Admissions
+            </Styled.TaskCardTitle>
+            <Styled.TaskCardDescription>
+              The number of admission events to the agency’s jurisdiction in
+              which the person has been adjudicated.
+            </Styled.TaskCardDescription>
+            <Styled.TaskCardActionLinksWrapper>
+              <Styled.TaskCardActionLink>
+                Set Metric Availability
+              </Styled.TaskCardActionLink>
+              <Styled.TaskCardActionLink>Upload Data</Styled.TaskCardActionLink>
+              <Styled.TaskCardActionLink>New Record</Styled.TaskCardActionLink>
+              <Styled.TaskCardActionLink>Publish</Styled.TaskCardActionLink>
+            </Styled.TaskCardActionLinksWrapper>
+          </Styled.TaskCard>
+        </Styled.OpenTasksContainer>
+        <Styled.Submenu>
+          <Styled.SubmenuButton>
+            <GearIcon />
+            Agency Settings
+          </Styled.SubmenuButton>
+          <Styled.SubmenuButton>
+            <SettingsIcon />
+            Metric Settings
+          </Styled.SubmenuButton>
+          <Styled.SubmenuButton>
+            <OpenLinkIcon />
+            Justice Counts Website
+          </Styled.SubmenuButton>
+        </Styled.Submenu>
+      </Styled.ContentContainer>
     </Styled.HomeContainer>
   );
-};
+});
