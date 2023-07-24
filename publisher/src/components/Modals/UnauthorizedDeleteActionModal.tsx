4d4f37c6451cc06df91b1a478283632a1a5e4afe
Mahmoud <mahmoud@Mahmouds-MBP.cable.rcn.com>
2023-07-24 13:02:27 -0500

    Create enum for types of assets mapped to their display name and pass in as props

--- publisher/src/components/Modals/UnauthorizedDeleteActionModal.tsx
+++ publisher/src/components/Modals/UnauthorizedDeleteActionModal.tsx
@@ -17,10 +17,6 @@
 
 import { Modal } from "@justice-counts/common/components/Modal";
 import React from "react";
-import { useLocation } from "react-router-dom";
-
-import { removeAgencyFromPath } from "../../utils";
-import { REPORT_LOWERCASE, REPORTS_LOWERCASE } from "../Global/constants";
 
 import { ResourceType, resourceTypeToDisplayName } from ".";
 
