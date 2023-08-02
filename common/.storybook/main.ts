19093feecd73755df9105e08c19b5d51c866301a
Alfonso Gober <alfonso@recidiviz.org>
2023-08-01 21:39:54 -0700

    add story

--- common/.storybook/main.ts
+++ common/.storybook/main.ts
@@ -61,6 +61,7 @@ const config: StorybookConfig = {
         https: false,
         stream: false,
         crypto: false,
+        module: false,
         "crypto-browserify": require.resolve("crypto-browserify"), //if you want to use this module also don't forget npm i crypto-browserify
       };
     return config;
