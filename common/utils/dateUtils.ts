22ff3a8dc8965289db82217dcb69b493832d14f8
Mahmoud <mahmoud@Mahmouds-MBP.cable.rcn.com>
2023-08-15 10:47:20 -0500

    Fix lint

--- common/utils/dateUtils.ts
+++ common/utils/dateUtils.ts
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // =============================================================================
-
+// eslint-disable-next-line import/no-relative-packages
 import { REPORT_CAPITALIZED } from "../../publisher/src/components/Global/constants";
 import { ReportFrequency } from "../types";
 
