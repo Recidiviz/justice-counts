acb0f2f74e7297195f0ad35d85d933d8cae024c4
jwhrec <129418088+jwhrec@users.noreply.github.com>
2023-10-03 22:10:45 -0400

    update account set up page text

--- publisher/src/components/HelpCenter/Guides/AccountSetupGuide.tsx
+++ publisher/src/components/HelpCenter/Guides/AccountSetupGuide.tsx
@@ -23,20 +23,21 @@ export const AccountSetupGuide = () => (
   <>
     <Styled.SectionWrapper>
       <Styled.SectionParagraph>
-        Within agency settings you can update information about your agency
-        including your URL and the description of your agency.
+      Within agency settings you can update:
+        <UL>agency users</UL>
+        <UL>the description of your agency</UL>
+        <UL>jurisdictions covered by your agency</UL>
+
       </Styled.SectionParagraph>
     </Styled.SectionWrapper>
     <Styled.SectionWrapper>
       <Styled.SectionParagraph>
-        {`{Screenshot of jurisdictions}`} You may also edit the jurisdictions
-        that your agency covers and specify areas included and excluded.
+      When setting jurisdiction, you can specify areas that are included and excluded. 
       </Styled.SectionParagraph>
     </Styled.SectionWrapper>
     <Styled.SectionWrapper>
       <Styled.SectionParagraph>
-        If you need to update your agency name, state, or sector, please contact
-        justice-counts-support@csg.org.
+      If you need to update your agency name, state, or sector, please contact <a href="mailto:justice-counts-support@csg.org">justice-counts-support@csg.org</a>. Some users may also have the ability to add new users to the agency, by specifying the name and email address. 
       </Styled.SectionParagraph>
     </Styled.SectionWrapper>
   </>
