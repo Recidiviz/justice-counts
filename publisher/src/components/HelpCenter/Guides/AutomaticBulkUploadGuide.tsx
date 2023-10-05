17ef0ca5d389d80fd1a059f076a128e6f9de8242
jwhrec <129418088+jwhrec@users.noreply.github.com>
2023-10-04 21:17:56 -0400

    Update AutomaticBulkUploadGuide.tsx

--- publisher/src/components/HelpCenter/Guides/AutomaticBulkUploadGuide.tsx
+++ publisher/src/components/HelpCenter/Guides/AutomaticBulkUploadGuide.tsx
@@ -23,35 +23,25 @@ export const AutomatedBulkUploadGuide = () => (
   <>
     <Styled.SectionWrapper>
       <Styled.SectionParagraph>
-        Automated Bulk Upload is a new feature that allows you to upload data to
-        our system without logging in. This is done using Secure File Transfer
-        Protocol (SFTP), which is a secure way to transfer files over a
-        network.The files must be Excel workbooks or CSV sheets. The files must
-        comply with our technical specification. You can find more information
-        about the technical specification and download custom templates here
-        [hyperlink to Michelle’s work].
+        Automated Data Upload is a feature that allows you to upload data to Justice Counts with the bulk upload format without logging in to Publisher. This is done using Secure File Transfer Protocol (SFTP), which is a secure way to transfer files over a network. This can be done through the following steps:
+        <li>Export your data as a CSV or Excel file in the Justice Counts bulk upload format. </li>
+        <li>Use Secure File Transfer Protocol (SFTP) to upload your file to our server. To initiate this process, please contact{" "} 
+          <a href="mailto:justice-counts-support@csg.org">
+          justice-counts-support@csg.org
+          </a>.
+        </li>
+        <li>Optionally, create a script to automate steps 1 and 2 to upload data at a regular cadence.</li>
       </Styled.SectionParagraph>
     </Styled.SectionWrapper>
 
     <Styled.SectionWrapper>
-      <Styled.SectionTitle>Confirmation Email</Styled.SectionTitle>
+      <Styled.SectionTitle>SFTP Process</Styled.SectionTitle>
       <Styled.SectionParagraph>
-        Once you have uploaded a file via this new feature, you will receive a
-        confirmation email from no-reply@justice-counts.org The email will
-        include information on whether or not your upload was successful. This
-        email will also include a link to a review page, where you can review
-        and publish your uploaded data. If your upload resulted in errors and
-        warnings due to data/formatting issues, the email will also include a
-        link to an errors and warnings page.
-      </Styled.SectionParagraph>
-    </Styled.SectionWrapper>
-
-    <Styled.SectionWrapper>
-      <Styled.SectionTitle>How to use SFTP</Styled.SectionTitle>
-      <Styled.SectionParagraph>
-        First, contact us at [help email address] to request a username and
+        Secure File Transfer Protocol (SFTP) allows you to transfer data with a high level of security. To begin the process, contact us at <a href="mailto:justice-counts-support@csg.org">
+          justice-counts-support@csg.org
+        </a> to request a username and
         password for our SFTP server. Once we have confirmed that your account
-        has been created, do the following to connect to our SFTP server.
+        has been created, run the following command from your command line to connect to our SFTP server.
       </Styled.SectionParagraph>
       <Styled.SectionParagraph>
         <pre>
@@ -75,13 +65,12 @@ export const AutomatedBulkUploadGuide = () => (
       <Styled.SectionParagraph>
         This is a standard message that your computer’s internal security system
         will show whenever you connect to an external server for the first time.
-        Our server is secure, type yes to connect.
+        Our server is secure, so type yes to connect.
       </Styled.SectionParagraph>
       <Styled.SectionParagraph>
         <ol>
           <li>
-            You will be prompted for your password, please enter the password
-            provided for you by Justice Counts.
+            When prompted for your password, enter the password provided for you in step 1.
           </li>
           <li>
             To see what files are currently uploaded to the SFTP server, type{" "}
@@ -104,6 +93,9 @@ export const AutomatedBulkUploadGuide = () => (
           </li>
           <li>
             To leave sftp, type <code>exit</code>.
+          </li>
+          <li>
+            Once you have uploaded a file, you will receive a confirmation email.
           </li>
         </ol>
       </Styled.SectionParagraph>
