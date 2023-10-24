5ab43f533908fb4345c59e8e313c83afd7c3d46a
Mahmoud <mahmoud@Mahmouds-MBP.cable.rcn.com>
2023-10-24 13:41:46 -0500

    Fix issue between dates in Publisher and dashboards - still needs more work - noticed it affected another metrics bar graph in a strange way - oi

--- common/components/DataViz/utils.ts
+++ common/components/DataViz/utils.ts
@@ -237,7 +237,6 @@ export const fillTimeGapsBetweenDatapoints = (
   if (data.length === 0) {
     return data;
   }
-  console.log(startingMonth);
 
   const isAnnual = data[0].frequency === "ANNUAL";
   const increment = isAnnual ? incrementYear : incrementMonth;
@@ -259,9 +258,15 @@ export const fillTimeGapsBetweenDatapoints = (
     isAnnual ? startingMonth || 0 : lastDate.getMonth(),
     lastDate.getFullYear()
   );
-  console.log(lastDate, startingMonth);
+
   for (let i = 0; i < data.length; i += 1) {
     const currentDate = new Date(data[i].start_date);
+    /**
+     * If `startingMonth` is provided, update the month of `lastDate` to the `startingMonth`
+     * provided to sync the gap date month with the metric's configured starting month.
+     */
+    if (startingMonth !== undefined) lastDate.setMonth(startingMonth, 1);
+
     const timeInterval =
       data[0].frequency === "MONTHLY"
         ? thirtyOneDaysInSeconds
@@ -271,14 +276,6 @@ export const fillTimeGapsBetweenDatapoints = (
     let offset = 0;
     while (currentDate.getTime() - lastDate.getTime() > timeInterval) {
       lastDate = increment(lastDate);
-      if (startingMonth !== undefined) {
-        /**
-         * Since each loop sets `lastDate` to `currentDate`, this will explicitly re-set
-         * the month of `lastDate` to the `startingMonth` provided (if provided) and not have it
-         * default to the month of `currentDate`.
-         */
-        lastDate.setMonth(startingMonth, 1);
-      }
       dataWithGapDatapoints.splice(i + offset + totalOffset, 0, {
         start_date: lastDate.toUTCString(),
         end_date: increment(lastDate).toUTCString(),
@@ -291,7 +288,7 @@ export const fillTimeGapsBetweenDatapoints = (
     totalOffset += offset;
     lastDate = currentDate;
   }
-  console.log("hi", dataWithGapDatapoints);
+
   return dataWithGapDatapoints;
 };
 
