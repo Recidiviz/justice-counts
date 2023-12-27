2830ba1f533e01b575c2a36af6696c9d08d0cfb2
Mahmoud <mahmoud@Mahmouds-MBP.atlanticbb.net>
2023-12-27 11:39:05 -0500

    Production only

--- publisher/src/index.tsx
+++ publisher/src/index.tsx
@@ -53,10 +53,7 @@ Sentry.init({
   tracesSampleRate: 1.0,
 
   // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: [
-    "localhost",
-    /^https:\/\/publisher\.justice-counts\.org\/api/,
-  ],
+  tracePropagationTargets: [/^https:\/\/publisher\.justice-counts\.org\/api/],
 
   // Capture Replay for 10% of all sessions,
   // plus for 100% of sessions with an error
