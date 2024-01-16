6695898d4533acc999fb8437c8903f4da76f4da9
Mahmoud <mahmoud@Mahmouds-MBP.cable.rcn.com>
2024-01-16 08:38:04 -0600

    Revert filter change

--- agency-dashboard/src/stores/AgencyDataStore.ts
+++ agency-dashboard/src/stores/AgencyDataStore.ts
@@ -200,7 +200,7 @@ class AgencyDataStore {
         const result = await response.json();
         runInAction(() => {
           this.agency = result.agency;
-          this.metrics = result.metrics.filter((metric) => metric.enabled);
+          this.metrics = result.metrics;
         });
       } else {
         const error = await response.json();
