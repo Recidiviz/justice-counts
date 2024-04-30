9728bb473b0d0437a11fb0e55f5b9c1c8f4d1c9e
Michelle Orden <michelleorden@Michelles-MBP.attlocal.net>
2024-04-30 11:30:22 -0500

    trim whitespace from agency name

--- publisher/src/stores/AdminPanelStore.ts
+++ publisher/src/stores/AdminPanelStore.ts
@@ -351,7 +351,7 @@ class AdminPanelStore {
   }
 
   updateAgencyName(name: string) {
-    this.agencyProvisioningUpdates.name = name;
+    this.agencyProvisioningUpdates.name = name.trim();
   }
 
   updateStateCode(stateCode: StateCodeKey | null) {
@@ -465,7 +465,7 @@ class AdminPanelStore {
       | User
       | UserWithAgenciesByID
       | AgencyWithTeamByID
-      | AgencyTeamMember
+      | AgencyTeamMember,
   >(list: T[], order: "ascending" | "descending" = "ascending"): T[] {
     return list.sort((a, b) => {
       if (order === "descending") {
@@ -510,7 +510,7 @@ class AdminPanelStore {
       | Agency
       | AgencyWithTeamByID
       | UserWithAgenciesByID
-      | AgencyTeamMember
+      | AgencyTeamMember,
   >(obj: Record<string, T[]>) {
     return AdminPanelStore.sortListByName(
       Object.values(obj).flatMap((item) => item)
