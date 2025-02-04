ab9d05f76f063c0581c13df09ec6e49f4625e899
nasaownsky <someuser.ivn@gmail.com>
2025-02-04 18:36:34 +0400

    Fix sizing of user cards when searching team members

--- publisher/src/components/AdminPanel/AdminPanel.styles.tsx
+++ publisher/src/components/AdminPanel/AdminPanel.styles.tsx
@@ -335,7 +335,7 @@ export const ButtonWrapper = styled.div`
 
 export const TeamMembersContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
   gap: 10px;
   margin-top: 12px;
 `;
